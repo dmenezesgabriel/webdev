81c839b7fa1062a34d62e8f845b45c196c52b50c
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-09-07 11:56:29 -0300

    chore: update todo api

---
+++ apps/todo-api/src/infrastructure/http/fastify/routes/users/logout.ts
@@ -0,0 +1,12 @@
+import { type FastifyReply, type FastifyRequest } from "fastify";
+
+export async function logout(request: FastifyRequest, reply: FastifyReply) {
+  reply.clearCookie("refreshToken", {
+    path: "/",
+    httpOnly: true,
+    sameSite: "strict",
+    secure: true,
+  });
+
+  return reply.status(200).send({ message: "Logged out successfully" });
+}
--- apps/todo-api/src/infrastructure/http/fastify/routes/users/users-routes.ts
+++ apps/todo-api/src/infrastructure/http/fastify/routes/users/users-routes.ts
@@ -6,6 +6,7 @@ import { verifyUserRole } from "@/infrastructure/http/fastify/middlewares/verify
 import { authenticate } from "./authenticate";
 import { createUser } from "./create-user";
 import { listUsers } from "./list-users";
+import { logout } from "./logout";
 import { profile } from "./profile";
 import { refreshToken } from "./refresh-token";
 
@@ -13,6 +14,7 @@ export async function userRoutes(app: FastifyInstance) {
   app.post("/users", createUser);
   app.post("/sessions", authenticate);
   app.patch("/token/refresh", refreshToken);
+  app.post("/sessions/logout", logout);
 
   app.get("/users", { onRequest: [verifyUserRole("ADMIN")] }, listUsers);
   app.get("/me", { onRequest: [verifyJWT] }, profile);
