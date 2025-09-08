20382bd4b83fd4f42fe1593e01bce0c9978c59e9
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-09-08 15:56:57 -0300

    chore: update todo-angular styles

--- apps/todo-angular/src/app/app.stories.ts
+++ apps/todo-angular/src/app/app.stories.ts
@@ -3,6 +3,7 @@ import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
 
 const meta: Meta<AppComponent> = {
   title: 'App',
@@ -10,7 +11,7 @@ const meta: Meta<AppComponent> = {
   tags: ['autodocs'],
   decorators: [
     moduleMetadata({
-      declarations: [SignUpComponent],
+      declarations: [SignUpComponent, HeaderComponent],
       imports: [ReactiveFormsModule],
     }),
   ],
