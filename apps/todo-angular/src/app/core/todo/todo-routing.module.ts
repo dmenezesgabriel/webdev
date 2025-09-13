5fec43267c0c56b46ca963e4abe2dbc56e5af42b
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-09-13 17:32:13 -0300

    chore: fix imports

--- apps/todo-angular/src/app/core/todo/todo-routing.module.ts
+++ apps/todo-angular/src/app/core/todo/todo-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoComponent } from './todo.component';
-import { AuthGuard } from '../auth/auth.guard';
+import { AuthGuard } from '../../auth/auth.guard';
 import { TodosResolver } from './todo.resolver';
 
 const routes: Routes = [
