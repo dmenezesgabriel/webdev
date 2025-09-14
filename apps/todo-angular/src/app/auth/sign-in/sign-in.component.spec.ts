2bebe0eccdbea1ecf62a587128f9bba37ef2746f
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-09-14 10:59:34 -0300

    chore: configure jest

--- apps/todo-angular/src/app/auth/sign-in/sign-in.component.spec.ts
+++ apps/todo-angular/src/app/auth/sign-in/sign-in.component.spec.ts
@@ -1,33 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignInComponent } from './sign-in.component';
-import { AuthService } from '../auth.service';
-import { Router } from '@angular/router';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { provideHttpClient } from '@angular/common/http';
 import { CardComponent } from '../../shared/card/card.component';
 
 describe('SignInComponent', () => {
   let component: SignInComponent;
   let fixture: ComponentFixture<SignInComponent>;
-  let mockAuthService: jasmine.SpyObj<AuthService>;
-  let mockRouter: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
-    mockAuthService = jasmine.createSpyObj('AuthService', [
-      'loginUser',
-      'saveToken',
-    ]);
-    mockRouter = jasmine.createSpyObj('Router', ['navigate']);
-
     await TestBed.configureTestingModule({
       declarations: [SignInComponent, CardComponent],
       imports: [ReactiveFormsModule],
-      providers: [
-        provideHttpClient(),
-        { provide: AuthService, useValue: mockAuthService },
-        { provide: Router, useValue: mockRouter },
-      ],
+      providers: [provideHttpClient()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SignInComponent);
