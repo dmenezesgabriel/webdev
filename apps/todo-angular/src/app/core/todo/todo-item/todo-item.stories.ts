ae2f81763c2739f1de5d3ae9db91d6db8412ccc0
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-09-14 18:51:12 -0300

    chore: todo item story tests

--- apps/todo-angular/src/app/core/todo/todo-item/todo-item.stories.ts
+++ apps/todo-angular/src/app/core/todo/todo-item/todo-item.stories.ts
@@ -1,6 +1,7 @@
 import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TodoItemComponent } from './todo-item.component';
+import { userEvent, within, expect, fn } from 'storybook/test';
 
 const meta: Meta<TodoItemComponent> = {
   title: 'Todo/Todo-Item',
@@ -26,5 +27,19 @@ export const Default: Story = {
       completedAt: null,
       createdAt: new Date(),
     },
+    toggle: fn(),
+    delete: fn(),
+  },
+  play: async ({ canvasElement, args }) => {
+    const canvas = within(canvasElement);
+
+    const toggleCheckbox = canvas.getByRole('checkbox');
+    const deleteButton = canvas.getByRole('button', { name: /delete/i });
+
+    await userEvent.click(toggleCheckbox);
+    await userEvent.click(deleteButton);
+
+    expect(args.toggle).toHaveBeenCalledWith('1');
+    expect(args.delete).toHaveBeenCalledWith('1');
   },
 };
